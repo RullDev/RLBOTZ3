648bbe705be1550f26850d9f977a3b5881a9618a
RullDev <105213064+RullDev@users.noreply.github.com>
2022-10-31 14:37:23 +0700

    Update __menfess-ans.js

--- plugins/__menfess-ans.js
+++ plugins/__menfess-ans.js
@@ -21,9 +21,9 @@ handler.all = async function (m) {
 	console.log({ text: m.text })
 	if (mf && (m.text === 'Balas Pesan' || m.text === '') && m.quoted?.mtype == 'buttonsMessage') return m.reply("Silahkan kirim pesan balasan kamu.")
 	
-	let txt = `Hai kak @${mf.dari.split('@')[0]}, kamu menerima balasan nih.\n\nPesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
+	let txt = `Hai kak @${mf.dari.split('@')[0]}, 📮 Hai kamu menerima balasan crush kamu nih.\n\nPesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
 	await this.reply(mf.dari, txt, null).then(() => {
-		m.reply('Berhasil mengirim balasan.')
+		m.reply('Berhasil mengirim balasan, Semoga gak NT Yah🗿')
 		delay(2000)
 		delete this.menfess[mf.id]
 		return !0
