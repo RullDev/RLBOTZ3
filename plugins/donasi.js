af78719579f00460f9b2f18a15a328afa8ce8eaf
RullDev <105213064+RullDev@users.noreply.github.com>
2022-11-05 05:25:17 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,11 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Gopay  [083160327945]
-│ • Dana  [083848128003]
-│ • Saweria  [https://saweria.co/asepbotz]
+│ • Gopay  [087753812675]
+│ • Pulsa  [087753812675]
+│ • Saweria  [https://saweria.co/RullZy]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6283160327945
+│ > Ingin donasi? Wa.me/6287753812675
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
