ce17ae7affec9c615afb5b0eaf5777a1c1f72d97
RullDev <105213064+RullDev@users.noreply.github.com>
2022-11-05 09:51:18 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -5,7 +5,7 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 
 let handler = async function (m, { text, usedPrefix, command }) {
 
-let info = `❏ *📮 Script RullBOT*\n│• *Script :*\n│↳ Private ya hihihiヾ(〃^∇^)ﾉ\n│• *Node_Modules:*\n│↳ https://semawur.com/mwuPPdYT\n│↳ Ato tekan *npm  i/yarn install*\n│• *Base :* \n│↳ github.com/bochilgaming/games-wabot-md/\n *• Free Panel:*\n http://bit.ly/3VehFES┗──────────═┅═──────────\n\n• *Mau script? Chat Owner!*\n• Jika menemukan bug di script, harap lapor owner\n• Websites RullBOT: https://bit.ly/3eggcxd Jangan, lupa join grup ya\n• Bot ini free untuk digunakan di grup ini, kalo mau memasukan BOT ke grup lain minta izin owner dulu ya:)`
+let info = `❏ *📮 Script RullBOT*\n│• *Script :*\n│↳ Private ya hihihiヾ(〃^∇^)ﾉ\n│• *Node_Modules:*\n│↳ https://semawur.com/mwuPPdYT\n│↳ Ato tekan *npm  i/yarn install*\n│• *Base :* \n│↳ github.com/bochilgaming/games-wabot-md/\n *• Free Panel:*\n http://bit.ly/3VehFES\n ┗──────────═┅═──────────\n\n• *Mau script? Chat Owner!*\n• Jika menemukan bug Bot, harap lapor owner\n• Websites RullBOT: https://bit.ly/3eggcxd Jangan, lupa join grup ya\n• Bot ini free untuk digunakan di grup ini, kalo mau memasukan BOT ke grup lain minta izin owner dulu ya:)`
 let kled = `https://telegra.ph/file/ff053a2dff6c14bcb0578.jpg`
 let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 
