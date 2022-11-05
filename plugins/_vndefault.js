97b664770065423402901bca5a6bce248358ea3c
RullDev <105213064+RullDev@users.noreply.github.com>
2022-11-05 16:56:21 +0700

    Tiriz

---
+++ plugins/_vndefault.js
@@ -0,0 +1,16 @@
+import fs from'fs'
+let { MessageType } = (await import('@adiwajshing/baileys')).default
+let handler = async (m, { conn }) => {
+let whmods = fs.readFileSync('./media/vndefault.opus') 
+conn.sendFile(m.chat, whmods, '', '', m, true)
+}
+
+handler.command = /^(menu|help)$/i
+
+handler.limit = false
+handler.mods = false 
+handler.premium = false
+handler.group = false
+handler.private = false
+
+export default handler
