2ce2e144b95f7075aa8b12e7ca4c88a1ab091213
RullDev <105213064+RullDev@users.noreply.github.com>
2022-11-06 18:15:25 +0700

    V3 Alpha1

--- plugins/stopjadibot.js
+++ plugins/stopjadibot.js
@@ -5,8 +5,19 @@ let handler  = async (m, { conn }) => {
     conn.close()
   }
 }
-handler.help = ['stopjadibot']
+handler.help = ['berhenti','stop']
 handler.tags = ['jadibot']
-handler.command = /^(stopjadibot)$/i
+handler.command = /^(berhenti|stop)$/i
 handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
 module.exports = handler
+
