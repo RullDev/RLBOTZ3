86a87b7623b207086ab6705b6e775e244f1fcad9
RullDev <105213064+RullDev@users.noreply.github.com>
2022-11-12 13:33:11 +0700

    Add files via upload

---
+++ plugins/animewebsite.js
@@ -0,0 +1,21 @@
+let handler = async m => m.reply(`
+「 Website Anime 」
+
+⫹⫺ Website: https://anoboy.ninja/
+⫹⫺ Website2: -
+⫹⫺ Website3: -
+⫹⫺ *Website anime gratis tanpa iklan dan watermark*
+
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['websiteanime']
+handler.tags = ['info']
+handler.command = /^websiteanime$/i
+handler.premium = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+export default handler 
