6bf2ce03ebaac239f0bbb7edc3dde23047187936
RullDev <105213064+RullDev@users.noreply.github.com>
2022-11-13 11:31:10 +0700

    Update info-paystore.js

--- plugins/info-paystore.js
+++ plugins/info-paystore.js
@@ -2,30 +2,21 @@ let handler = async (m, { conn }) => {
 	//-----PRICE
 //sewa
 let sh = '7'
-let sn = '20'
-let ss = '40'
-let sp = '75'
-let sv = '5'
+let sn = '15'
+let dn = '10'
 //premium
 let ph = '5'
 let pn = '20'
-let pp = '40'
-let pv = '50'
 let ppm = '70'
 let info = `
 ╭━━━━「 *SEWA BOT* 」
-┊⫹⫺ *Hemat:* _${sh}k/Run Bot (1 minggu)_
-┊⫹⫺ *Normal:* _${sn}k/Run Bot (1 bulan)_
-┊⫹⫺ *Standar:* _${ss}k/Run Bot (2 bulan)_
-┊⫹⫺ *Vip:* _${sp}k/Run Bot (12 bulan)_                                                      
-┊⫹⫺ *Ganti Nama Bot+Owner:* = _${sv}k_
+┊⫹⫺ *Hemat:* _${sh}k/Grup (1 minggu)_
+┊⫹⫺ *Normal:* _~${sn}k~ Diskon: ${dn}k/Grup (1 bulan)_                                                    
 ╰═┅═━––––––๑
 
-╭━━━━「 *PREMIUM* 」
+╭━━━━「 *PREMIUM USER* 」
 ┊⫹⫺ *Hemat:* _${ph}k (1 minggu)_
 ┊⫹⫺ *Normal:* _${pn}k (1 bulan)_
-┊⫹⫺ *Pro:* _${pp}k (4 bulan)_
-┊⫹⫺ *Vip:* _${pv}k (8 bulan)_                                               
 ┊⫹⫺ *Permanent:* = _${ppm}k (Unlimited)_
 ╰═┅═━––––––๑
 
@@ -46,19 +37,14 @@ const sections = [
    {
 	title: `${htjava} SEWA BOT ✦-------`,
 	rows: [
-	    {title: "🔖 𝗛𝗘𝗠𝗔𝗧", rowId: '.order *Paket:* HEMAT • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sh + 'k (1 minggu)' },
-	    {title: "🔖 𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.order *Paket:* NORMAL • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sn + 'k (1 bulan)' },
-	{title: "🔖 𝗦𝗧𝗔𝗡𝗗𝗔𝗥", rowId: '.order *Paket:* STANDAR • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + ss + 'k (2 bulan)' },
-	{title: "🔖 𝗩𝗜𝗣", rowId: '.order *Paket:* VIP • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sp + 'k (12 bulan)' },
-	{title: "🔖 ɢᴀɴᴛɪ ɴᴀᴍᴀʙᴏᴛ", rowId: '.order *Paket:* Ganti Nama Bot+Owner • Sewa', description: '𝗣𝗿𝗶𝗰𝗲: ' + sv },
+	    {title: "🔖 𝗛𝗘𝗠𝗔𝗧", rowId: '.order *Paket:* HEMAT • Sewa Bot Grup', description: '𝗣𝗿𝗶𝗰𝗲: ' + sh + 'k (1 minggu)' },
+	    {title: "🔖 𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.order *Paket:* NORMAL • Sewa Bot Grup', description: '𝗣𝗿𝗶𝗰𝗲: ' + dn + 'k (1 bulan)' },
 	]
     }, {
     title: `${htjava} PREMIUM ✦-------`,
 	rows: [
 	    {title: "🌟 𝗛𝗘𝗠𝗔𝗧", rowId: '.order *Paket:* HEMAT • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + ph + 'k (1 minggu)' },
 	    {title: "🌟 𝗡𝗢𝗥𝗠𝗔𝗟", rowId: '.order *Paket:* NORMAL • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pn + 'k (1 bulan)' },
-	{title: "🌟 𝗣𝗥𝗢", rowId: '.order *Paket:* PRO • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pp + 'k (4 bulan)' },
-	{title: "🌟 𝗩𝗜𝗣", rowId: '.order *Paket:* VIP • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + pv + 'k (8 bulan)' },
 	{title: "🌟 𝗣𝗘𝗥𝗠𝗔𝗡𝗘𝗡𝗧", rowId: '.order *Paket:* PERMANENT • Premium', description: '𝗣𝗿𝗶𝗰𝗲: ' + ppm + 'k (UNLIMITED)' },
 	]
     },
