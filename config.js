58bb6317856b2e21658cf93aa255842f0e8a45ec
RullKhrl <105213064+RullDev@users.noreply.github.com>
2022-11-21 13:44:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -86,7 +86,7 @@ global.APIs = { // API Prefix
   ana: 'https://anabotofc.herokuapp.com/',
   adiisus: 'https://adiixyzapi.herokuapp.com',
   violetics : 'https://violetics.pw', 
-  males : 'https://malesin.xyz'
+  males : 'https://api.lolhuman.xyz'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
