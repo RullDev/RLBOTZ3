12e40e30ab12871eeec0e9ec616f013f7fa53a96
RullKhrl <khoirulmustofa767@gmail.com>
2023-01-07 12:22:35 +0700

    Add files via upload

---
+++ plugins/buat-ngopi.js
@@ -0,0 +1,40 @@
+import fs from 'fs'
+import { createHash } from 'crypto'
+import fetch from 'node-fetch'
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+
+let handler = async function (m, { text, usedPrefix, command }) {
+
+let info = `❏ *📮 Donasi Buat Ngopi Bang:)\n Gopay:6287753812675\n Yang Donasi Semoga Banyak Rezekinya Aminn\n MyWeb: https://apiruulzz.my.id`
+let kled = `https://telegra.ph/file/ff053a2dff6c14bcb0578.jpg`
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+let buttonMessage= {
+'document':{'url':'https://www.google.com'},
+'mimetype':global.ddocx,
+'fileName':'Script.pdf',
+'fileLength':9999999999999,
+'pageCount':999,
+'contextInfo':{
+'forwardingScore':1000,
+'isForwarded':true,
+'externalAdReply':{
+'mediaUrl':sig,
+'mediaType':2,
+'previewType':'pdf',
+'title':'Bot Esm',
+'body':titlebot,
+'thumbnail':await(await fetch(kled)).buffer(),
+'sourceUrl':'https://www.instagram.com/taikucing'}},
+'caption':info,
+'footer':botdate,
+'buttons':[
+{'buttonId':'.menu','buttonText':{'displayText':'Menu'},'type':1}
+],
+'headerType':6}
+    await conn.sendMessage(m.chat, buttonMessage, { quoted:m})
+}
+
+handler.customPrefix = /^(.trakteer)$/i
+
+export default handler
