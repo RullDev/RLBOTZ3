9811469c3d52e3c71be2c6df06ea1d213b90a01e
RullKhrl <khoirulmustofa767@gmail.com>
2023-01-07 12:02:10 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    let res = (await axios.get(API('males', '/tiktok', { url: args[0] } ))).data;
+    let res = (await axios.get(API('males', 'https://api.vhtear.com/tiktok_no_wm?link=https://www.tiktok.com/@goodponsel/video/6913737919450729729&apikey=APIKEY', { url: args[0] } ))).data;
     if (res.status != 200) throw res.message;
     if (!res) throw res.message;
     conn.sendButtonVid(m.chat, res.video, `*🎋Deskripsi:* ${res.title}\n${res.author ? `*🎐Pembuat Video:* ${res.author}` : '\n' }`.trim(), 'Cara simpan digalery:\n1. Download dulu videonya\n2. Buka terus klik titik 3 pojok kanan atas\n3. lalu klik simpan digaleri!', '🎈Menu', usedPrefix + 'menu', m)
